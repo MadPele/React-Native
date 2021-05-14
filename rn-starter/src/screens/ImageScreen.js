2d88973fe4c8c27722ec757ea3aae9822e181ae1
Andrzej Derela <andrzejderela@gmail.com>
2021-05-14 19:31:23 +0200

    ImageDetails and CounterScreen add

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {View, Text, StyleSheet } from 'react-native'
+import ImageDetail from '../components/ImageDetail'
 
 const ImageScreen = () => {
     return (
-        <Text>Image Screen</Text>
+        <View>
+            <ImageDetail title='Forest' score={10} imageSource={require('../../assets/forest.jpg')} />
+            <ImageDetail title='Beach' score={5} imageSource={require('../../assets/beach.jpg')} />
+            <ImageDetail title='Mountain' score={8} imageSource={require('../../assets/mountain.jpg')} />
+        </View>
     )
 }
 
