7e526e95cd3b90d56c3cae2c3be4a057064bb625
Andrzej Derela <andrzejderela@gmail.com>
2021-05-16 16:30:28 +0200

    add SearchBar

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {View, TextInput, StyleSheet} from "react-native";
+import {Feather} from '@expo/vector-icons'
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+    return(
+        <View style={styles.backgroundStyle}>
+            <Feather name='search' style={styles.iconStyle} />
+            <TextInput
+                style={styles.inputStyle}
+                placeholder='Search'
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
+                autoCapitalize='none'
+                autoCorrect={false}
+                onEndEditing={() => onTermSubmit()}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: 'white',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: "row",
+        marginTop: 10
+    },
+    inputStyle: {
+        fontSize: 18,
+        flex: 1
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 10
+    }
+})
+
+export default SearchBar
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet} from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+
+    const [term, setTerm] = useState('')
+
     return(
         <View>
-            <Text>Search Screen</Text>
+            <SearchBar
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+                onTermSubmit={() => console.log('term was submitted')}
+            />
+            <Text>{term}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'white'
+    }
+})
 
 export default SearchScreen
